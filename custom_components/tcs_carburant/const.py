fa82a463677c7037aa8a11163393275ee6183c56
froguinou <froguinou@live.fr>
2026-05-25 01:02:07 +0200

    Sync stable production version and remove cache files

--- custom_components/tcs_carburant/const.py
+++ custom_components/tcs_carburant/const.py
@@ -1,6 +1,12 @@
 DOMAIN = "tcs_carburant"
 
-URL = "https://firestore.googleapis.com/v1/projects/gas-prices-prod/databases/(default)/documents:runQuery"
+FIREBASE_API_KEY = "AIzaSyCQ8f6sXb1gYIiv5rlHKeZ2EVMzC-anzIU"
+
+URL = (
+    "https://firestore.googleapis.com/v1/projects/"
+    "gas-prices-prod/databases/(default)/documents:runQuery"
+    f"?key={FIREBASE_API_KEY}"
+)
 
 FUEL_TYPES = ["SP95", "SP98", "DIESEL"]
 
